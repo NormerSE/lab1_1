8ff8954d59806893bece4993c0a640192794cd25
NormerSE <normer3500@gmail.com>
2025-09-06 16:09:23 +0500

    Добавлен main.py с алгоритмом Евклида

---
+++ main.py
@@ -0,0 +1,9 @@
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+if __name__ == "__main__":
+    x = int(input("Введите первое число: "))
+    y = int(input("Введите второе число: "))
+    print("НОД =", gcd(x, y))
